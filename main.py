fc15569a6166fa811c08c07a09e704bd84fdc3a4
naziahkhan <naziah.khan@stonybrook.edu>
2023-05-13 02:52:13 +0000

    this is an API for MN diagnoses descriptions

--- main.py
+++ main.py
@@ -2,32 +2,33 @@ from flask import Flask, request
 import pandas as pd
 
 df = pd.read_csv('./data/diagnoses2019-2.csv')
+df.columns
 
 app = Flask(__name__)
 
 @app.route('/', methods=["GET"])
 def home():
-    return 'this is a API service for MN ICD code details'
+    return 'this is an API service for MN diagnosis description details'
 
 @app.route('/preview', methods=["GET"])    
 def preview():
-    top10rows = df.head(1)
+    top10rows = df.head(10)
     result = top10rows.to_json(orient="records")
     return result
 
-@app.route('/icd/<value>', methods=["GET"])    
-def icdcode(value):
-    #R73
+@app.route('/diag/<value>', methods=["GET"])    
+def diagdes(value):
+    #Psoriasis
     print('value: ', value)
-    filtered = df[df['principal_diagnosis_code'] == value]
+    filtered = df[df['diagnosis_description'] == value]
     if len(filtered) <= 0:
         return 'There is nothing here'
     else:
         return filtered.to_json(orient="records")
 
-@app.route('/icd/<value>/sex/<value2>')    
-def icdcode2(value, value2):
-    filtered = df[df['principal_diagnosis_code'] == value]
+@app.route('/diag/<value>/sex/<value2>')    
+def diagdes2(value, value2):
+    filtered = df[df['diagnosis_description'] == value]
     filtered2 = filtered[filtered['sex'] == value2]
     if len(filtered2) <= 0:
         return 'There is nothing here'
